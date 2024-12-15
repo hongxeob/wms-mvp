317e5a33cf4d146356e68454787917d1aaefa3e5
hongxeob <sjun2918@naver.com>
2024-12-15 20:31:19 +0900

    2. create test case registerProduct

--- src/test/java/org/example/wmsmvp/product/feature/RegisterProductTest.java
+++ src/test/java/org/example/wmsmvp/product/feature/RegisterProductTest.java
@@ -1,4 +1,20 @@
 package org.example.wmsmvp.product.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class RegisterProductTest {
+
+    @Test
+    @DisplayName("상품을 등록한다.")
+    void registerProduct() throws Exception {
+
+        //given
+
+
+        //when
+
+        //then
+
+    }
 }
