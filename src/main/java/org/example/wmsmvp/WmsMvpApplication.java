82e94523990ed5a7cc6c92d86364ace196096359
hongxeob <sjun2918@naver.com>
2024-12-15 20:25:38 +0900

    first commit

---
+++ src/main/java/org/example/wmsmvp/WmsMvpApplication.java
@@ -0,0 +1,13 @@
+package org.example.wmsmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wmsmvp/WmsMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wmsmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
