11b5dd2c5348b2ceb2293e0fdfa20a7f5c035594
hongxeob <sjun2918@naver.com>
2024-12-16 21:28:41 +0900

    10. create RegisterProductApi

--- src/test/java/org/example/wmsmvp/product/feature/RegisterProductTest.java
+++ src/test/java/org/example/wmsmvp/product/feature/RegisterProductTest.java
@@ -1,15 +1,11 @@
 package org.example.wmsmvp.product.feature;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.example.wmsmvp.common.ApiTest;
-import org.example.wmsmvp.product.domain.Category;
 import org.example.wmsmvp.product.domain.ProductRepository;
-import org.example.wmsmvp.product.domain.TemperatureZone;
+import org.example.wmsmvp.product.feature.api.RegisterProductApi;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,41 +21,8 @@ public class RegisterProductTest extends ApiTest {
     void registerProduct() throws Exception {
 
         //given
-        final String name = "name";
-        final String code = "code";
-        final String description = "description";
-        final String brand = "brand";
-        final String maker = "maker";
-        final String origin = "origin";
-        final Long weightInGrams = 1000L;
-        final Long widthInMillimeters = 100L;
-        final Long heightInMillimeters = 100L;
-        final Long lengthInMillimeters = 100L;
-
-        RegisterProduct.Request request = new RegisterProduct.Request(
-                name,
-                code,
-                description,
-                brand,
-                maker,
-                origin,
-                Category.ELECTRONICS,
-                TemperatureZone.ROOM_TEMPERATURE,
-                weightInGrams, //그램
-                widthInMillimeters, //너비 mm
-                heightInMillimeters, //높이 mm
-                lengthInMillimeters //길이 mm
-        );
-
-        //when
-//        registerProduct.request(request);
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when()
-                .post("/products")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+        RegisterProductApi api = new RegisterProductApi();
+        api.request();
         //then
 
         assertThat(productRepository.findAll()).hasSize(1);
