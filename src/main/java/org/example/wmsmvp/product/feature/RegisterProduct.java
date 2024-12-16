e0fe8d29a1f0cc6c81305c8fc2d049b64ab1910a
hongxeob <sjun2918@naver.com>
2024-12-16 21:13:26 +0900

    8. RegisterProduct API로 변경

--- src/main/java/org/example/wmsmvp/product/feature/RegisterProduct.java
+++ src/main/java/org/example/wmsmvp/product/feature/RegisterProduct.java
@@ -1,8 +1,16 @@
 package org.example.wmsmvp.product.feature;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.example.wmsmvp.product.domain.*;
-import org.springframework.util.Assert;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RegisterProduct {
     private final ProductRepository productRepository;
 
@@ -10,49 +18,54 @@ public class RegisterProduct {
         this.productRepository = productRepository;
     }
 
-    public void request(Request request) {
+    @PostMapping("/products")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void request(@RequestBody Request request) {
         Product product = request.toDomain();
         productRepository.save(product);
     }
 
     public record Request(
+            @NotBlank(message = "상품명은 필수입니다.")
             String name,
+
+            @NotBlank(message = "상품 코드는 필수입니다.")
             String code,
+
+            @NotBlank(message = "상품 설명은 필수입니다.")
             String description,
+
+            @NotBlank(message = "브랜드명은 필수입니다.")
             String brand,
+
+            @NotBlank(message = "제조사는 필수입니다.")
             String maker,
+
+            @NotBlank(message = "원산지는 필수입니다.")
             String origin,
+
+            @NotNull(message = "카테고리는 필수입니다.")
             Category category,
+
+            @NotNull(message = "보관 온도는 필수입니다.")
             TemperatureZone temperatureZone,
+
+            @NotNull(message = "무게는 필수입니다.")
+            @Min(value = 0, message = "무게는 0보다 커야 합니다.")
             Long weightInGrams,
+
+            @NotNull(message = "너비는 필수입니다.")
+            @Min(value = 0, message = "너비는 0보다 커야 합니다.")
             Long widthInMillimeters,
+
+            @NotNull(message = "높이는 필수입니다.")
+            @Min(value = 0, message = "높이는 0보다 커야 합니다.")
             Long heightInMillimeters,
+
+            @NotNull(message = "길이는 필수입니다.")
+            @Min(value = 0, message = "길이는 0보다 커야 합니다.")
             Long lengthInMillimeters
     ) {
-        public Request {
-            Assert.hasText(name, "상품명은 필수입니다.");
-            Assert.hasText(code, "상품 코드는 필수입니다.");
-            Assert.hasText(description, "상품 설명은 필수입니다.");
-            Assert.hasText(brand, "브랜드명은 필수입니다.");
-            Assert.hasText(maker, "제조사는 필수입니다.");
-            Assert.hasText(origin, "원산지는 필수입니다.");
-
-            Assert.notNull(category, "카테고리는 필수입니다.");
-            Assert.notNull(temperatureZone, "보관 온도는 필수입니다.");
-
-            Assert.notNull(weightInGrams, "무게는 필수입니다.");
-            Assert.isTrue(weightInGrams > 0, "무게는 0보다 커야 합니다.");
-
-            Assert.notNull(widthInMillimeters, "너비는 필수입니다.");
-            Assert.isTrue(widthInMillimeters > 0, "너비는 0보다 커야 합니다.");
-
-            Assert.notNull(heightInMillimeters, "높이는 필수입니다.");
-            Assert.isTrue(heightInMillimeters > 0, "높이는 0보다 커야 합니다.");
-
-            Assert.notNull(lengthInMillimeters, "길이는 필수입니다.");
-            Assert.isTrue(lengthInMillimeters > 0, "길이는 0보다 커야 합니다.");
-        }
-
         public Product toDomain() {
             return new Product(
                     name,
--- src/test/java/org/example/wmsmvp/product/feature/RegisterProductTest.java
+++ src/test/java/org/example/wmsmvp/product/feature/RegisterProductTest.java
@@ -1,23 +1,34 @@
 package org.example.wmsmvp.product.feature;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.example.wmsmvp.product.domain.Category;
 import org.example.wmsmvp.product.domain.ProductRepository;
 import org.example.wmsmvp.product.domain.TemperatureZone;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RegisterProductTest {
+    @LocalServerPort
+    private int port;
 
-    private RegisterProduct registerProduct;
+    @Autowired
     private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
-        productRepository = new ProductRepository();
-        registerProduct = new RegisterProduct(productRepository);
+        //포트 설정
+        if (RestAssured.UNDEFINED_PORT == RestAssured.port) {
+            RestAssured.port = port;
+        }
     }
 
     @Test
@@ -50,10 +61,18 @@ public class RegisterProductTest {
                 heightInMillimeters, //높이 mm
                 lengthInMillimeters //길이 mm
         );
-        //when
-        registerProduct.request(request);
 
+        //when
+//        registerProduct.request(request);
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/products")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
         //then
+
         assertThat(productRepository.findAll()).hasSize(1);
     }
 
