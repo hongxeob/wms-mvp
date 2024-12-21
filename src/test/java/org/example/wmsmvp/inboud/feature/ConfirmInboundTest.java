70c532e9bc3fe57ef73b1f4b18e76898ca0f917b
hongxeob <sjun2918@naver.com>
2024-12-21 19:44:04 +0900

    5. confirm inbound 구현

--- src/test/java/org/example/wmsmvp/inboud/feature/ConfirmInboundTest.java
+++ src/test/java/org/example/wmsmvp/inboud/feature/ConfirmInboundTest.java
@@ -1,10 +1,19 @@
 package org.example.wmsmvp.inboud.feature;
 
 import org.example.wmsmvp.inboud.domain.Inbound;
+import org.example.wmsmvp.inboud.domain.InboundItem;
 import org.example.wmsmvp.inboud.domain.InboundRepository;
+import org.example.wmsmvp.inboud.domain.InboundStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConfirmInboundTest {
 
@@ -14,7 +23,8 @@ public class ConfirmInboundTest {
 
     @BeforeEach
     void setUp() {
-        confirmInbound = new ConfirmInbound();
+        inboundRepository = Mockito.mock(InboundRepository.class);
+        confirmInbound = new ConfirmInbound(inboundRepository);
     }
 
     @Test
@@ -22,16 +32,39 @@ public class ConfirmInboundTest {
     void confirmInbound() throws Exception {
         //given
         Long inboundNo = 1L;
+        final Inbound inbound = new Inbound(
+                "상품명",
+                "상품설명",
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                List.of(
+                        new InboundItem(
+                                ProductFixture.aProduct().build(),
+                                1L,
+                                1500L,
+                                "설명"
+                        )
+                )
+        );
+        Mockito.when(inboundRepository.findById(inboundNo))
+                .thenReturn(Optional.of(inbound));
 
         //when
         confirmInbound.request(inboundNo);
 
         //then
-//        assertThat(inboundRepository.findById(inboundNo).get().getStatus()).isEqualTo("");
+        assertThat(inbound.getStatus()).isEqualTo(InboundStatus.CONFIRMED);
 
     }
 
     private class ConfirmInbound {
+
+        private final InboundRepository inboundRepository;
+
+        private ConfirmInbound(final InboundRepository inboundRepository) {
+            this.inboundRepository = inboundRepository;
+        }
+
         public void request(final Long inboundNo) {
             final Inbound inbound = inboundRepository.findById(inboundNo).orElseThrow();
 
