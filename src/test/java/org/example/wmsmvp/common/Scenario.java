f4f36cf096475d49c2ba5a52aa76de1133e0a152
hongxeob <sjun2918@naver.com>
2024-12-21 20:24:51 +0900

    10. import bug fix

--- src/test/java/org/example/wmsmvp/common/Scenario.java
+++ src/test/java/org/example/wmsmvp/common/Scenario.java
@@ -1,5 +1,6 @@
 package org.example.wmsmvp.common;
 
+import org.example.wmsmvp.inboud.feature.api.RegisterInboundApi;
 import org.example.wmsmvp.product.feature.api.RegisterProductApi;
 
 public class Scenario {
