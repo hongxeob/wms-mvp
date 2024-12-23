81c84e754b4e29912109b0f0db0b86e8375e3340
hongxeob <sjun2918@naver.com>
2024-12-23 21:22:49 +0900

    2. edit error message

--- src/main/java/org/example/wmsmvp/inboud/domain/InboundRepository.java
+++ src/main/java/org/example/wmsmvp/inboud/domain/InboundRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface InboundRepository extends JpaRepository<Inbound, Long> {
     default Inbound getBy(Long inboundNo) {
         return findById(inboundNo).orElseThrow(
-                () -> new IllegalArgumentException("inbound not found"));
+                () -> new IllegalArgumentException("해당 입고가 존재하지 않습니다."));
     }
 }
